4c300b42b5e70e1613378d5a19ef7dab97b6a7f1
soumik12345 <19soumik.rakshit96@gmail.com>
2023-01-05 18:31:11 +0000

    updated wandb run name pattern + added run tags

--- convert_semantic_kitti.py
+++ convert_semantic_kitti.py
@@ -1,6 +1,7 @@
 import argparse
 import shutil
 
+import ray
 import wandb
 from lidar_baselines.dataloader.semantic_kitti import SemanticKITTIConverter
 
@@ -25,15 +26,22 @@ if __name__ == "__main__":
     )
     args = parser.parse_args()
 
-    run_name = "numpy-conversion/semantic-kitti/"
+    run_name = "semantic-kitti/"
     run_name += f"{args.sequence_id}/"
-    run_name += f"split-{args.lower_bound_index}-{args.upper_bound_index}"
+    run_name += f"{args.lower_bound_index}-{args.upper_bound_index}"
 
     with wandb.init(
         project="point-cloud-voxelize",
         entity="geekyrakshit",
         name=run_name,
         job_type="numpy-conversion",
+        tags=[
+            "semantic-kitti",
+            "numpy-conversion",
+            f"sequence-{args.sequence_id}",
+            f"split-{args.lower_bound_index}-{args.upper_bound_index}",
+        ],
+        config=args.__dict__,
     ):
         converter = SemanticKITTIConverter(
             artifact_address="geekyrakshit/point-cloud-voxelize/semantic-kitti:v1",
